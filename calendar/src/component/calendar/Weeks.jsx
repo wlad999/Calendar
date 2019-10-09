6436050fe2bba00237f31e558fb66e85e732ef36
ваше имяwlad999 <wladmail25@gmeil.com>
2019-10-09 14:11:39 +0300

    hours

--- calendar/src/component/calendar/Weeks.jsx
+++ calendar/src/component/calendar/Weeks.jsx
@@ -5,31 +5,101 @@ import React from "react";
 // };
 class Weeks extends React.Component {
   state = {
-    days: ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    days: ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"],
+    daysWeek: [],
+    hours: [
+      "12:00 AM",
+      "2:00 AM",
+      "4:00 AM",
+      "6:00 AM",
+      "8:00 AM",
+      "10:00 AM",
+      "12:00 PM",
+      "2:00 PM",
+      "4:00 PM",
+      "6:00 PM",
+      "8:00 PM",
+      "10:00 PM"
+    ]
   };
 
   getWeekDay = () => {
-    let date = new Date();
-    // let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-    return this.state.days[date.getDay()];
+    let date = new Date(2019, 9, 31);
+    let week = [
+      { day: "SUN", num: 0 },
+      { day: "MON", num: 0 },
+      { day: "TUE", num: 0 },
+      { day: "WED", num: 0 },
+      { day: "THU", num: 0 },
+      { day: "FRI", num: 0 },
+      { day: "SAT", num: 0 }
+    ];
+    let numWeekDay = date.getDay();
+    let getDateAgo = (date, days) => {
+      let dateCopy = new Date(date);
+      dateCopy.setDate(date.getDate() - days);
+      return dateCopy.getDate();
+    };
+
+    let newWeek = week.map((el, i) => {
+      el.num = getDateAgo(date, numWeekDay - i);
+      return el;
+    });
+
+    if (this.state.daysWeek.length === 0) {
+      this.setState({ daysWeek: newWeek });
+    }
+    return console.log(newWeek);
+
+    // let month = this.state.month[date.getMonth()];
+    // let weekDay = this.state.days[date.getDay()];
+    // let numMonth = date.getDate();
+    // let year = date.getFullYear();
+    // let countDaysInMonth = 0;
+    // let daysInMonth = (month, year) => {
+    //   countDaysInMonth = new Date(year, month + 1, 0).getDate();
+    // };
+    // daysInMonth(numMonth, year);
+    // return `${weekDay}, ${numDay}, ${month}, ${year}, this month have ${countDaysInMonth} days`;
   };
+
   render() {
     let dayStyle = {
       display: "flex",
       border: "2px solid grey",
       padding: "3px",
-      width: "80%",
+      // width: "80%",
       "justify-content": "center"
     };
+    let hours = {
+      display: "flex",
+      border: "2px solid grey",
+      padding: "3px",
+      width: "20%",
+      "justify-content": "center",
+      height: "75.5px"
+    };
+    this.getWeekDay();
+
     return (
       <>
         <div style={dayStyle}>
-          {this.state.days.map(el => (
-            <span style={dayStyle}>{el}</span>
+          <div style={hours}>
+            <div>HOUER</div>
+            {}
+          </div>
+          {this.state.daysWeek.map(el => (
+            <div>
+              <div style={dayStyle}>{el.day}</div>
+              <div style={dayStyle}>{el.num}</div>
+              {this.state.daysWeek.map(el => (
+                <div>{el.day}</div>
+              ))}
+            </div>
           ))}
         </div>
         <div>Weeks</div>
-        <div>today is {this.getWeekDay()}</div>
+        <div>{this.getWeekDay()}</div>
       </>
     );
   }
